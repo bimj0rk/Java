436e8c203b3549bfd86609017013dbf918fb0d45
bimj0rk <aicampean@gmail.com>
2022-01-15 21:54:48 +0200

    c

--- PL/PL_Lab_10/src/Account.java
+++ PL/PL_Lab_10/src/Account.java
@@ -45,31 +45,3 @@ class Account {
         return "Account:" + "id=" + id + ", balance=" + balance + ", owner=" + owner;
     }
 }
-
-class SavingsAccount extends Account{
-
-    private double interest;
-
-    public SavingsAccount(long balance, String owner, double interestRate) {
-        super(balance, owner);
-        this.interest = interestRate;
-    }
-
-    public SavingsAccount(String owner, double interestRate){
-        super(owner);
-        this.interest = interestRate;
-    }
-
-    public void applyInterest(){
-        deposit(getBalance() * this.interest);
-    }
-
-    public double getInterest() {
-        return interest;
-    }
-
-    @Override
-    public String toString() {
-        return super.toString() + ", interest=" + interest;
-    }
-}
